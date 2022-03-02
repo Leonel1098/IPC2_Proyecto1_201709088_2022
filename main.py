c4523433285b63d6de68aa447bf51f3d8162f61c
Leonel1098 <gonzalezleonel1098@gmail.com>
2022-03-01 23:27:41 -0600

    Menú

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
-from lxml import etree as t
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-from xml.etree import ElementTree as ET
+from xml.etree import ElementTree as data
 
 
+def Menu():
+    while True:
+        print("-----MENÚ-----")
+        print(" 1. Cargar XML ")
+        print(" 2. Elegir Patrón ")
+        print(" 3. Mostrar Gráfica del Patrón ")
+        print(" 4. Seleccionar Código de Patrón ")
+        print(" 5. Salir ")
+        opción = input("Elija una opción para continuar: ")
+        
+        if opción == "1":
+            print("XML")
+            load_file("archivoprueba.xml")
+        elif opción == "2":
+            print("Patrón")
+        elif opción == "3":
+            print ("Gráfica")
+        elif opción == "4":
+            print("Código")
+        elif opción == "5":
+            print("Vuelve Pronto :) ")
+            break
+        else:
+            print("Ingrese una opción válida.... ")
 
 def load_file(ruta):
     #Tk().withdraw()
-    tree = t.parse(ruta)
-    elements = tree.load_file()
-    filename = askopenfilename()
-    xml_tree = ET.parse(filename)
-    xml_root = xml_tree.getroot()
-    print(xml_tree)
+    tree = data.parse(ruta)
+    xml_root = tree.getroot()
+    print(xml_root)
 
-load_file()
+    print("Atributos")
+    for element in xml_root:
+        for subelement in element:
+            print(subelement.attrib)
+    print("Valores")
+    for element in xml_root:
+        for subelement in element:
+            print(subelement.tag)
+            print(subelement.text)
+
+
+Menu()
